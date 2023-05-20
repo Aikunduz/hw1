7ef4a5d733fb7594de01a3b5df830ba1c16a88f9
Aikunduz <aikunduz.baltabaeva@gmail.com>
2023-05-20 12:27:14 +0600

    Initial Commit

---
+++ CLASS 3 MONTH/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CLASS 3 MONTH
+//
+//  Created by Aikunduz on 5/20/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
